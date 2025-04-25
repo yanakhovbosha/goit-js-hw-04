89436464fbb35f23e31042842dad6f081ff6bb60
Yana Hovbosha <aemec4373@gmail.com>
2025-04-25 12:23:41 +0300

    add task-2 file

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,19 @@
+const profile = {
+    username: "Jacob",
+  playTime: 300,
 
+  changeUsername(newName) {
+        this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`
+  }
+};
 
 console.log(profile.getInfo());
 
